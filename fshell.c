2fd23f2f3888fadcb5eb75a8f7402f3bd92ac7e7
Hamza Eldafrawy <hamza.eldefrawy4@gmail.com>
2025-04-16 11:28:46 -0400

    femto shell has been updtated

--- fshell.c
+++ fshell.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    
-    char input[1024];
-    char *command;
-    
-    while(1){
+    char input[10240];
+    char *cmd;
+
+    while (1) {
         
-        //prompt
-        printf("أؤمر معلم بدك ياه لحمة ولا جاج؟");
-        printf(" > ");
+        // prompt
+        printf("أؤمر معلم بدك ياه لحمة ولا جاج؟ > ");
+
+        // read input
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            printf("\n");
+            return 0;
+        }
         
-        //
-        if(fgets(input, sizeof(input), stdin) == NULL){
+        input[strcspn(input, "\n")] = '\0';
+        
+        // skip empty
+        if (strlen(input) == 0) {
+            printf("\n");
             continue;
         }
-        
+
+        // extract command manually
+        cmd = strtok(input, " ");   
+
         //
-        input[strcspn(input, "\n")] = '\0';
-        command = strtok(input, " ");
-        
-        //
-        if(strcmp(command, "exit") == 0){
-            printf("Good Bye\n");
-            break;
-        }
-        
-        //
-        if(command != NULL && strcmp(command, "echo") == 0){
-            char *rest = input + strlen(command) + 1;
+        if (strcmp(cmd, "exit") == 0) {
+            printf("\nGood Bye\n");
+            return 0;
+        } else if (strcmp(cmd, "echo") == 0) {
+            char *rest = input + strlen(cmd) + 1;
             if(rest != NULL)
-                printf("%s\n", rest);
+                printf("\n%s\n", rest);
             else
                 printf("\n");
+        } else{
+            printf("\nInvalid command\n");
         }
-        else
-            printf("Invalid command\n");
     }
+
     return 0;
 }
